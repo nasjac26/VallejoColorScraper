9c82d71d33d9714eafeba1622f7ef0d9eee0eaf7
Jacob Nasrallah <JacoboNasrallah@gmail.com>
2024-01-18 13:53:15 -0800

    Made ordered stack of reference numbers and paint names using while loop

--- paintScrape/Program.cs
+++ paintScrape/Program.cs
@@ -5,38 +5,78 @@ using Fizzler.Systems.HtmlAgilityPack;
 using System.IO;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Drawing;
 
 namespace VallejoColorScraper
 {
     class Program
     {
-        public class Row
+        /*public class Row
         {
-            /*public string Name { get; set; }
-            public string ReferenceNum { get; set; }*/
+            *//*public string Name { get; set; }
+            public string ReferenceNum { get; set; }*//*
         }
+
+        public class Paint
+        {
+            public string Name { get; set; }
+            public string Reference { get; set; }
+            
+            public Paint(string name, string reference)
+            {
+                Name = name;
+                Reference = reference;
+            }
+        }*/
+
         static void Main(string[] args)
         {
             var html = @"https://acrylicosvallejo.com/en/category/hobby/game-color-en/";
 
             HtmlWeb web = new HtmlWeb();
-            //targeting first paint color element name
             var htmlDoc = web.Load(html);
             var paintNamesNodeList = htmlDoc.DocumentNode.QuerySelectorAll("h2[class='woocommerce-loop-product__title']");
             var paintReferencesNodeList = htmlDoc.DocumentNode.QuerySelectorAll("div[class='referencia']");
+            int count = 0;
+            
+            Stack<string> paintNameStack = new Stack<string>();
+            Stack<string> paintReferenceStack = new Stack<string>();
+            Stack<string> combinedStack = new Stack<string>();
 
-            string[,] paintInfo = { { "foo", "bar" } };
+            string[] arr = new string[paintNameStack.Count*2];
 
-            foreach (var paintName in paintNamesNodeList)
-                {
-                    Console.WriteLine(paintName.InnerText);
-                    Console.WriteLine(paintInfo[0,0]);
-                    Console.WriteLine(paintInfo[0,1]);
+            //Making a stack from nodelist of color names
+            foreach (var item in paintNamesNodeList)
+            {
+                count++;
+                string stringifiedInfo = item.InnerText;
+                paintNameStack.Push(stringifiedInfo);
             }
 
-            foreach (var paintReference in paintReferencesNodeList)
+            //Making a stack from nodelist of color reference numbers
+            foreach (var item in paintReferencesNodeList)
             {
-                Console.WriteLine(paintReference.InnerText);
+                string stringifiedInfo = item.InnerText.Trim();
+                paintReferenceStack.Push(stringifiedInfo);
+            }
+
+            //Combining two stacks so in reference number > name format
+            while (paintNameStack.Count > 0 || paintReferenceStack.Count > 0)
+            {
+                if (paintNameStack.Count > 0)
+                { 
+                    combinedStack.Push(paintNameStack.Pop());
+                }
+
+                if (paintReferenceStack.Count > 0)
+                {
+                    combinedStack.Push(paintReferenceStack.Pop());
+                }
+            }
+
+            foreach (var item in combinedStack)
+            {
+                Console.WriteLine(item);
             }
         }
     }
